9f3ede19e845e9b8a6ac8942386011389d136294
Vaibhav Dudhal <97098598+vaibhavdudhal@users.noreply.github.com>
2023-10-23 11:35:23 +0530

    Update script.js

--- wetaher app/script.js
+++ wetaher app/script.js
@@ -115,7 +115,7 @@ async function fetchUserWeatherInfo(coordinates)
 
     // now call APi
     try{
-        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+        const response=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
         const data= await response.json();
 
         if (!data.sys) {
